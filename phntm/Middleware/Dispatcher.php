8f3215d1b808d9c1db4c14642430013fcda046b3
Billy Chubb <billy@bchubb.co.uk>
2024-07-10 00:53:04 +0100

    split dispatch from router

---
+++ phntm/Middleware/Dispatcher.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bchubbweb\PhntmFramework\Middleware;
+
+use Bchubbweb\PhntmFramework\Pages\PageInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+
+class Dispatcher implements \Psr\Http\Server\MiddlewareInterface
+{
+    /**
+     * Dispatcher constructor.
+     * setup the response factory
+     *
+     * @param ResponseFactoryInterface $responseFactory
+     */
+    public function __construct(private ResponseFactoryInterface $responseFactory) {}
+
+    /**
+     * Renders the page provided by the router, or return a response with an
+     * appropriate status code if not.
+     *
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(
+        \Psr\Http\Message\ServerRequestInterface $request, 
+        \Psr\Http\Server\RequestHandlerInterface $handler
+    ): \Psr\Http\Message\ResponseInterface {
+        $page = $request->getAttribute('page', 404);
+
+        if (!$page instanceof PageInterface) {
+            return $this->responseFactory->createResponse($page);
+        }
+
+        // dont process the middleware stack as this is last in the chain
+        $response = $this->responseFactory->createResponse();
+
+        // render the page content
+        $body = $page->render($request->getAttribute('symfonyRequest'));
+
+        // return response with page body
+        return $response->withBody($body);
+    }
+}
